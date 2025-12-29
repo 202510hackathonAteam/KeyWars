5c776c0f362179efa6e9f3275c38f2b453932ab9
mai051 <aco_miya@outlook.jp>
2025-12-29 13:49:06 +0900

    Update: 使用しない関数の削除

--- backend/internal/util/cookie/cookie.go
+++ backend/internal/util/cookie/cookie.go
@@ -99,9 +99,3 @@ func ClearTokens(c echo.Context) {
 	ClearAccessToken(c)
 	ClearRefreshToken(c)
 }
-
-// SetCSRFToken は、CSRFトークンをCookieに設定
-func SetCSRFToken(c echo.Context, csrfToken string, expiry time.Duration) {
-	csrfCookie := newCookie("csrf_token", csrfToken, expiry)
-	c.SetCookie(csrfCookie)
-}
